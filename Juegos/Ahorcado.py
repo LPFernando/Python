508ce0971b7b96e97bdb3aae6558a984e6b50394
LPFernando <fernandosis.17.96@gmail.com>
2019-01-23 16:31:48 -0500

    Segundo avance del Ahorcado

--- Juegos/Ahorcado.py
+++ Juegos/Ahorcado.py
@@ -1,7 +1,64 @@
 #-*- coding: utf-8 -*
 
 from random import randint
-
+#imagenes para mostrar
+imagenes_ahorcado=['''
+    
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+  =========''','''
+    +---+
+    |   |
+    o   |
+        |
+        |
+        |
+  =========''','''
+  
+    +---+
+    |   |
+    o   |
+    |   |
+        |
+        |
+  =========''','''
+  
+    +---+
+    |   |
+    o   |
+   /|   |
+        |
+        |
+  =========''','''
+  
+    +---+
+    |   |
+    o   |
+   /|\  |
+        |
+        |
+  =========''','''
+  
+    +---+
+    |   |
+    o   |
+   /|\  |
+   /    |
+        |
+  =========''','''
+  
+    +---+
+    |   |
+    o   |
+   /|\  |
+   /\   |
+        |
+  ========='''
+  ] 
 #lista de palabras, gracias al metodo split
 
 palabras = '''hormiga babuino tejon murcielago oso castor camello gato
@@ -24,10 +81,12 @@ def obtener_intento(letras_probadas):
             print("Ingrese una letra, por favor.")
         elif intento in letras_probadas:
             print("Cambia de letra, por favor.")
-        elif intento not in "abcdefghijklmn�opqrstuvwxyz":
+        elif intento not in "abcdefghijklmnñopqrstuvwxyz":
             print("ingrese una letra")
         else:
             return intento
+        
+#***********************************************************************************
 
 print("A H O R C A D O")
 
@@ -35,9 +94,34 @@ letras_correctas=""
 letras_incorrectas=""
 
 palabra_secreta=obtener_palabra_Aleatoria(palabras)
-intento=obtener_intento(letras_correctas+letras_incorrectas)
+juego_terminado=False
 
-if intento in palabra_secreta:
-    print("adivinaste.....")
-else:
-    print("fallaste..")         
+while True:
+    
+    intento=obtener_intento(letras_correctas+letras_incorrectas)
+    
+    if intento in palabra_secreta:
+        #print("adivinaste.....")
+        letras_correctas=letras_correctas+intento
+        encontrado_todas_las_palabras=True
+        
+        for i in range(len(palabra_secreta)):
+            
+            if palabra_secreta[i] not in letras_correctas:
+                
+                encontrado_todas_las_palabras=False
+                break
+            
+        if encontrado_todas_las_palabras:
+            print("¡Has acertado!, la palabra secreta es "+palabra_secreta+" ¡Has Ganado!")
+            juego_terminado=True
+    else:
+        
+        letras_incorrectas=letras_incorrectas+intento
+        print("fallaste..")     
+          
+        if len(letras_incorrectas)==len(imagenes_ahorcado)-1:
+            
+            print("Has agotado tus intentos \n Despues de ",str(len(letras_incorrectas))," intentos")
+            juego_terminado=True
+            
